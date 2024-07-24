039f3ab63bc5772f803d748d74ae9fd516dcf3fb
snowcube <snow.cube@outlook.com>
2024-07-24 17:16:11 +0800

    basic function completed

--- frontend/src/components/FileFunction.vue
+++ frontend/src/components/FileFunction.vue
@@ -1,12 +1,22 @@
 <template>
-    <div class="btn-box">
-        <h3>文件上传：</h3>
-        <input class="file-input" type="file" @change="getFile($event)" />
-        <el-button type="primary" @click="upload">上传文件(POST)</el-button>
-        <h3>文件下载：</h3>
-        <el-button type="primary" @click="downloadLink">下载带链接文件(window.open)</el-button>
-        <el-button type="primary" @click="downloadBlobByGet">二进制流下载(GET)</el-button>
-        <el-button type="primary" @click="downloadBlobByPost">二进制流下载(POST)</el-button>
+    <div class="module-box">
+        <h3 class="box-title">模型输入</h3>
+        <div class="box-content">
+            <div id="file-select">
+                <input id="file-input" type="file" @change="getFile($event)" />
+                <label type="button" for="file-input" class="upload-btn">
+                    选择文件
+                </label>
+                <span class="file-name">{{fileName}}</span>
+            </div>
+            <el-button class="file-btn" type="primary" @click="upload">上传文件(POST)</el-button>
+        </div>
+    </div>
+    <div class="module-box">
+        <h3 class="box-title">模型输出</h3>
+        <div class="box-content">
+            <el-button class="file-btn" type="primary" @click="downloadLink">下载带链接文件(window.open)</el-button>
+        </div>
     </div>
 </template>
 
@@ -17,16 +27,17 @@
         data() {
             return {
                 file: null,
-                fileName: ""
+                fileName: "未选择文件"
             }
         },
         methods: {
             // 选取文件
             getFile(event) {
                 this.file = event.target.files[0];
+                alert(this.file.name);
             },
 
-            // 上传文件(POST)
+            // 上传文件 (POST)
             upload() {
                 let url = "http://localhost:8000/upload";
                 let formData = new FormData();
@@ -54,51 +65,84 @@
                     window.open("http://localhost:8000/download?filename=" + this.fileName);
                 }
             },
-
-            // 二进制流下载(GET)
-            // async downloadBlobByGet() {
-            //     let urlGet = "http://localhost:8000/download?filename=" + this.fileName;
-            //     let fileData = await axios.get(urlGet, { responseType: "blob" });
-            //     let URL = window.URL || window.webkitURL;
-            //     let downloadUrl = URL.createObjectURL(fileData.data);
-            //     let a = document.createElement("a");
-            //     a.href = downloadUrl;
-            //     a.download = this.fileName;//下载后文件名
-            //     a.click();
-            //     a = null;
-            //     downloadUrl && URL.revokeObjectURL(downloadUrl);
-            // },
-
-            // 二进制流下载(POST)
-            // async downloadBlobByPost() {
-            //     let urlPost = "http://localhost:3000/download/post/test";
-            //     let fileData = await axios({
-            //         method: "post",
-            //         url: urlPost, // 请求地址
-            //         data: { fileName: this.fileName }, // 参数
-            //         responseType: "blob" // 表明返回服务器返回的数据类型
-            //     })
-            //     let URL = window.URL || window.webkitURL;
-            //     let downloadUrl = URL.createObjectURL(fileData.data);
-            //     let a = document.createElement("a");
-            //     a.download = this.fileName;
-            //     a.href = downloadUrl;
-            //     a.click();
-            //     a = null;
-            //     downloadUrl && URL.revokeObjectURL(downloadUrl);
-            // },
         },
     }
 </script>
 
 <style scoped>
-    .btn-box {
-        padding: 20px;
+    .module-box {
+        /* padding: 20px; */
+        background-color: aqua;
     }
-    .el-button,
-    input {
+
+    .box-title {
+        background-color: brown;
+        font-family: Arial, Helvetica, sans-serif;
+        font-weight: bold;
+        color: white;
+        padding: 10px 30px;
+    }
+
+    .box-content {
+        padding: 30px;
+        background-color: grey;
+    }
+
+    #file-select {
+        height: 100px;
+    }
+
+    .upload-btn {
+        background-color: green;
+        /* padding: 0 10px; */
+        text-align: center;
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 1.1em;
+        /* font-weight: bold; */
+        color: white;
+        display: block;
+        width: 150px;
+        height: 50px;
+        line-height: 48px; /* 需为 height 减去上下边框 */
+        float: left;
+        border-style: solid;
+        border-color: green;
+        border-width: 1px 1px 1px 1px;
+        border-radius: 10px 0 0 10px;
+    }
+
+    .file-name {
+        display: block;
+        width: 200px;
+        font-size: 1.1em;
+        padding: 0 15px;
+        height: 50px;
+        line-height: 48px; /* 需为 height 减去上下边框 */
+        float: left;
+        border-style: solid;
+        border-width: 1px 1px 1px 0;
+        border-color: green;
+        border-radius: 0 10px 10px 0;
+    }
+
+    .file-btn {
         max-width: fit-content;
         display: block;
+        /* margin: 20px; */
+        background-color: palevioletred;
+        padding: 15px 30px;
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 1.1em;
+        /* font-weight: bold; */
+        color: white;
+    }
+
+    #file-input {
+        /* max-width: fit-content;
+        display: block;
         margin: 20px;
+        background-color: aliceblue;
+        padding: 10px; */
+        display: none;
     }
 </style>
